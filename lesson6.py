895e8f00c96549f0fc5eebc1309aafbb6cc11a67
Nver77 <nver.nver.pogosyan@mail.ru>
2021-09-23 21:18:34 +0500

    my second

---
+++ lesson6.py
@@ -0,0 +1,56 @@
+# age = int(input('tariq? '))
+# w = int(input('ashxatanqyin porc tari? '))
+# g = input('m? or w? ').upper()=='M'
+# e = input('education? ').upper()=='H'
+# res = 18 < age < 30 and 2 < w < 5 and (g or e)
+# print(res)
+
+# age = int(input('age? ')) > 18
+# print(not age)
+
+# x = 5
+# y = 5
+# print(x == y)
+# print(x is y)
+# print(x is not y)
+# print(x != y)
+
+# print(id(x))
+# print(id(y))
+
+# x = 5
+# x = 7
+# print(id(x))
+# print(id(y))
+
+
+# cars = 'Mercedes, Lexus, Toyota'
+# years = 2000, 2001, 2002
+# car = input('maknish? ')
+# year = int(input('taretiv? '))
+# res = car in cars and year in years
+# print('Menq unenq ayd meqenan',res)
+
+# x = 'Merdedes'
+# print(x.count('e'))
+
+# word = 'python'
+# print(word.count('p'))
+
+# word = 'my city is Yerevan'
+# res = word.replace('Yerevan','Paris')
+# print(res)
+
+# print(abs(-12))
+# print(abs(2-27))
+
+# a = input('Apple? ').upper() == 'Y'
+# k = input('Kivi? ').upper() == 'Y'
+# b = input('Banane? ').upper() == 'Y'
+# l = input('Light? ').upper() == 'Y'
+# res1 = a and not l
+# print('hanel Apple sarnaranic', res1)
+# res2 = k and not l
+# print('hanel Kivi sarnaranic', res2)
+# res3 = b and not l
+# print('hanel Banane sarnaranic', res3)
